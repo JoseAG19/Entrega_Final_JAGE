701f0b0e3e2c6d41f93b2a59c8ea7bd137d9de16
JoseAG19 <165836565+JoseAG19@users.noreply.github.com>
2024-05-28 10:14:13 -0500

    Update RealidadAumentada.py

--- pages/RealidadAumentada.py
+++ pages/RealidadAumentada.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title("Mundos y realidades aumentadas")
 st.write("En esta apartado vas a poder conocer parte del futuro de las ciudades; es decir, las ciudades virtuales. Accede al siguiente link para ver una muestra de una ciudad virtual: https://www.spatial.io/s/ElEscarneadors-Bespoke-Room-6655f093032da3b82131e2d9")
 st.image("cebraVR.png")
+st.subheader("Todas las formas de vida merecen una realidad aumentada")
